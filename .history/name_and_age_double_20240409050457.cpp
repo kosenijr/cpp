155e5a2a914f8263871ad3f77a9466d3306b4db1
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-04-09 05:15:09 -0400

    changed double files to month files and added new double file

--- .history/name_and_age_double_20240409050457.cpp
+++ .history/name_and_age_double_20240409050457.cpp
@@ -3,13 +3,13 @@
 // main function: integer return type
 int main()
 {
-    // prompt user to enter information
+    // first string output
     cout << "Please enter your first and age\n";
-    // declare variables for first name and age in years
+    // declaration of first name and age in years
     string first_name;
     int age_years; 
     cin >> first_name >> age_years; // read into first name and age in years": user puts in years.
-    int age_months = age_years * 12; // calculate age in months.
+    int age_months = age_years * 12; // new declaration for age in months.
     cout << "Hello, " << first_name << " (Age: " << age_months << " months old).\n";
     return 0;
 } 
