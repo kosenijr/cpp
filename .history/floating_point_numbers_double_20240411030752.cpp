52b73ff644bb2f78b1323084dce4af12dabe95dc
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-04-11 03:13:35 -0400

    saving floating-point numbers tasks

--- .history/floating_point_numbers_double_20240411030752.cpp
+++ .history/floating_point_numbers_double_20240411030752.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     cout << "Please enter a floating-point value:";
-    int n;
+    double n;
     cin >> n;
     cout << "n == " << n
     << "\nn+1 == " << n + 1
@@ -12,5 +12,5 @@ int main()
     << "\nn squared == " << n*n
     <<"\nhalf of n == " << n/2
     << "\nsquare root of n == " << sqrt(n)
-    << '\n';
+    << '\n'
 }
