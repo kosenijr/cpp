e32cf6dbcb68bd9d41d7182e070528bd63995988
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-04-29 02:36:32 -0400

    working on unsafe conversions

---
+++ .history/names_without_errors_20240429014232.cpp
@@ -0,0 +1,9 @@
+// example without errors
+#include "std_lib_facilities.h"
+
+int main() // first error: main should not be capitalized; should be lowercase.
+{
+    string s = "Goodbye, cruel world! "; // second error: string is to be lowercase as well.
+    cout << s << '\n'; // third error: the standard output stream, cout, should be lowercase; fourth error: "s" is uppercase; it should be lowercase.
+    // additional error: no return statement
+    }
