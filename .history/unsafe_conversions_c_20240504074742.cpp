a1501db2beb23d941048e00d595b09ad9e176ac3
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-05-04 07:48:28 -0400

    deleted third unsafe conversions file

---
+++ .history/unsafe_conversions_c_20240504074742.cpp
@@ -0,0 +1,24 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    double d = 0;
+    // input
+    while (cin >> d)
+    {
+        // variables
+        int i = d;
+        char c = i;
+        int i2 = c;
+        // couldn't use double and int with operand: type must match
+        // output
+        cout << "d == " << d
+             << "\ni == " << i
+             << "\ni2 == " << i2
+             << "\nchar: (" << c << ")"
+             << "\nremainder: " << remain1 << "\n";
+
+    }
+}
+
+// char cycles through values -128 through 127: 255 characters.
