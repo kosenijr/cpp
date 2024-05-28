683aac5df8439167f64436cf8c708f331b7438d6
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-05-28 16:30:21 -0400

    exercise #5 complete

---
+++ .history/double_to_int_for_ratios_20240527033002.cpp
@@ -0,0 +1,38 @@
+#include "std_lib_facilities.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countDigitsAfterDecimal(const string &number)
+{
+    size_t decimalPos = number.find('.');
+    if (decimalPos == string::npos)
+    {
+        // No decimal point found, return 0
+        return 0;
+    }
+
+    int digitCount = 0;
+    for (size_t i = decimalPos + 1; i < number.length(); ++i)
+    {
+        if (isdigit(number[i]))
+        {
+            digitCount++;
+        }
+    }
+    return digitCount;
+}
+
+int main()
+{
+    string number;
+    cout << "Enter a decimal number: ";
+    cin >> number;
+
+    int numDigitsAfterDecimal = countDigitsAfterDecimal(number);
+    int numAfterDecimal = number * pow(1, countDigitsAfterDecimal(number);
+    cout << "The number of digits after the decimal point is: " << numDigitsAfterDecimal << endl;
+
+    return 0;
+}
---
+++ .history/two_double_values_b_20240528141453.cpp
@@ -0,0 +1,48 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    // declare two objects with int type
+    double val1, val2;
+    // prompting for two integers
+    cout << "Please enter two values that are integers: \n";
+    // read into the two integers, respectively
+    cin >> val1 >> val2;
+
+    // double to int truncation
+    int val1_int = val1;
+    int val2_int = val2;
+
+    // implement gcd
+    int divisor = gcd(val1_int, val2_int);
+    int val1_simplified = val1_int/divisor;
+    int val2_simplified = val2_int/divisor;
+
+
+    // manipulations
+    // smaller and larger
+    if (val1 < val2)
+    {
+        cout
+            << "The smaller number: " << val1 << "\n"
+            << "The larger number: " << val2 << "\n"
+            << "The sum: " << val1 + val2 << "\n"
+            << "The difference: " << val2 - val1 << "\n"
+            << "The product: " << val1 * val2 << "\n";
+    }
+    else
+    {
+        cout
+            << "The smaller number: " << val2 << "\n"
+            << "The larger number: " << val1 << "\n"
+            << "The sum: " << val1 + val2 << "\n"
+            << "The difference: " << val1 - val2 << "\n"
+            << "The product: " << val1 * val2 << "\n";
+    };
+
+    // ratio
+    cout << "The ratio: " << val1_int << ":" << val2_int << "\n";
+
+    // return statement
+    return 0;
+}
