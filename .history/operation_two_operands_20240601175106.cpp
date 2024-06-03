96cd18bc69cd9892803ecfb374fb8bdbf6aef079
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-06-02 22:10:19 -0400

    exercise #10 is complete

---
+++ .history/operation_two_operands_20240601175106.cpp
@@ -0,0 +1,11 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    // declare string and double-typed objects;
+    string operation;
+    double var1, var2;
+    // prompt user to enter for input
+    cout << "Please enter an operation with the symbol or name of the operator first before the two variables:"
+    cin >> operation >> 
+}
