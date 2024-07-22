15eba0d7c9977e26a4394d7f0de12a17b58fa132
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-07-22 16:23:48 -0400

    will retry after much study

---
+++ .history/vertical_bar_drill_20240722124230.cpp
@@ -0,0 +1,42 @@
+/*
+Write a program that consists of a while-loop that (each time around
+the loop) reads in two ints and then prints them. Exit the program
+when a terminating '|' is entered.
+*/
+
+#include "std_lib_facilities.h"
+
+// function for converting char to int
+int char_to_int(char c)
+{
+    int i = (int)c;
+    return i;
+}
+
+int main()
+{
+    // declaration of ints and char
+    int a;
+    int b;
+    int i;
+
+    // while statement
+    while (true)
+    {
+        // prompt user for input
+        cout << "Please enter two integers: \n";
+        // read into two ints
+        cin >> a >> b;
+        if (a == false || b == false)
+        {
+            break;
+        }
+        else
+        {
+            // send to standard output stream
+            cout << a << " " << b << '\n';
+        }
+    }
+
+    return 0;
+}
