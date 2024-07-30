bde8fedb0c9e226f9ee178fb212ef68164b1ab60
U-KAYODE-LGG\Oseni <k.oseni.jr@gmail.com>
2024-07-30 05:03:00 -0400

    added a file

---
+++ .history/smaller_larger_equal_while_statement_with_doubles_20240729130037.cpp
@@ -0,0 +1,59 @@
+/*
+Change the program so that it uses doubles instead of ints.
+*/
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    // declare one char and two ints
+    char char1;
+    double dbl1, dbl2;
+
+    // set up while statement with true parameter
+    while (true)
+    {
+        // prompt user for input
+        cout << "Please enter two decimals or '|' to exit: \n";
+
+        // set up conditions
+        if (cin >> dbl1 >> dbl2)
+        {
+            // add a new condition
+            if (dbl1 < dbl2)
+            {
+                cout
+                    << "The smaller value is: " << fixed << setprecision(2) << dbl1 << '\n'
+                    << "The larger value is: " << fixed << setprecision(2) << dbl2 << '\n';
+            }
+            else if (dbl1 > dbl2)
+            {
+                cout
+                    << "The smaller value is: " << fixed << setprecision(2) << dbl2 << '\n'
+                    << "The larger value is: " << fixed << setprecision(2) << dbl1 << '\n';
+            }
+            else
+            {
+                cout << "The numbers are equal. \n";
+            };
+        }
+        else
+        {
+            // clear out the standard output stream
+            cin.clear();
+            // redirect input to char
+            cin >> char1;
+            // set up inner condition
+            if (char1 == '|')
+            {
+                break;
+            }
+            else
+            {
+                cout << "Incorrect input. \n";
+            };
+        };
+    };
+    // return statement
+    return 0;
+}
